6e1a4c5539865ca52f6ec7d48eefb10783ac2654
+ <+>
2022-09-04 15:09:18 -0700

    commit tree ec3c3dd4e07cc401c9124efd522b3e5fff2ac92b

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solutions in this file
+
+const array = []
+function writeCards(array, eventName) {
+  for (let i = 0; i < array.length; i++) {
+    array[i] = `Thank you, ${array[i]}, for the wonderful ${eventName} gift!`
+  }
+  return array
+}
+
+function countDown(number){
+  while (number >= 0)
+  console.log(number--)
+}
